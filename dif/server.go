b800ecb0cf53dc0ba082ce8fc98b22e08324687d
Sebastien Binet <binet@cern.ch>
2020-03-03 12:06:39 +0100

    dif: more meat

--- dif/server.go
+++ dif/server.go
@@ -42,16 +42,84 @@ func (srv *Server) scanDevices(ctx tdaq.Context) error {
 	for _, dev := range devs {
 		ctx.Msg.Infof("found DIF 0x%x", dev.ProdID)
 		srv.difs = append(srv.difs, dev.ID)
-
 		srv.devs[dev.ID] = dev
 	}
 
 	sort.Slice(srv.difs, func(i, j int) bool {
-		di := srv.difs[i]
-		dj := srv.difs[j]
-		return di < dj
+		return srv.difs[i] < srv.difs[j]
 	})
 
+	return nil
+}
+
+func (srv *Server) initialize(ctx tdaq.Context, id uint32) error {
+	if _, dup := srv.rdos[id]; dup {
+		ctx.Msg.Errorf("DIF 0x%x already registered", id)
+		return xerrors.Errorf("DIF 0x%x already registered", id)
+	}
+
+	dev, ok := srv.devs[id]
+	if !ok {
+		ctx.Msg.Errorf("DIF 0x%x not found in device map", id)
+		return xerrors.Errorf("DIF 0x%x not found in device map", id)
+	}
+
+	rdo, err := NewReadout(dev.Name, dev.ProdID, ctx.Msg)
+	if err != nil {
+		ctx.Msg.Errorf("could not create readout for DIF 0x%x (name=%q): %w",
+			id, dev.Name, err,
+		)
+		return xerrors.Errorf("could not create readout for DIF 0x%x (name=%q): %w",
+			id, dev.Name, err,
+		)
+	}
+
+	srv.rdos[id] = rdo
+
+	return nil
+}
+
+func (srv *Server) OnConfig(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /config command...")
+	err := srv.scanDevices(ctx)
+	if err != nil {
+		ctx.Msg.Errorf("could not scan devices: %+v", err)
+		return xerrors.Errorf("could not scan devices: %w", err)
+	}
+
+	return nil
+}
+
+func (srv *Server) OnInit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /init command...")
+	for id := range srv.devs {
+		dev := srv.devs[id]
+		err := srv.initialize(ctx, dev.ID)
+		if err != nil {
+			ctx.Msg.Errorf("could not initialize DIF 0x%x: %+v", dev.ID, err)
+			return xerrors.Errorf("could not initialize DIF 0x%x: %w", dev.ID, err)
+		}
+	}
+	return nil
+}
+
+func (srv *Server) OnReset(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /reset command...")
+	return nil
+}
+
+func (srv *Server) OnStart(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /start command...")
+	return nil
+}
+
+func (srv *Server) OnStop(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /stop command...")
+	return nil
+}
+
+func (srv *Server) OnQuit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /quit command...")
 	return nil
 }
 
